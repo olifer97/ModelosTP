77770556ec064f4b8891e1ba276adefee8300b20
Oli <olifer97@gmail.com>
2019-10-10 20:14:45 -0300

    nada

--- calcular_distancias.py
+++ calcular_distancias.py
@@ -5,10 +5,10 @@ import csv
 def haversine(lat1, lon1, lat2, lon2):
     R = 6372.8 # this is in miles.  For Earth radius in kilometers use 6372.8 km
 
-    dLat = lat2 - lat1
-    dLon = lon2 - lon1
-    lat1 = lat1
-    lat2 = lat2
+    dLat = radians(lat2 - lat1)
+    dLon = radians(lon2 - lon1)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
 
     a = sin(dLat/2)**2 + cos(lat1)*cos(lat2)*sin(dLon/2)**2
     c = 2*asin(sqrt(a))
