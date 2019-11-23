4cfd11bf82619d1c6c98c7813987c5a3d08af952
Oli <olifer97@gmail.com>
2019-11-23 15:47:13 -0300

    Modifico heuristica que ordene a los votantes por la suma de las distancias.

--- heuristicas/heuristica1.py
+++ heuristicas/heuristica1.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import random
 import csv
+import os
+import psutil
+
 
 #CONSTANTES DEL REDUCIDO
 N = 1134
@@ -16,13 +19,17 @@ def centros_cupos_minimos_completos(centros):
             return False
     return True
 
+def ordenar_votantes_por_distancia_summax(votants,dists):
+    return sorted(votants, key=lambda x: sum(dist for center,dist in dists[x]), reverse=True)
+
 def heuristica(dists, cupos):
     center_votant = {}
     votants = []
     for i in range(1,C+1):
-        center_votant[i] = []
+        center_votant[i] = [] #centro i vacio
     for i in range(1,N+1):
-        votants.append(i)
+       votants.append(i) #todos los votantes sin asignar
+    votants = ordenar_votantes_por_distancia_summax(votants,dists)
     
     #asignacion inicial
     while len(votants):
@@ -34,22 +41,25 @@ def heuristica(dists, cupos):
                     votants.remove(votant)
                     break
 
+    i = 0 #para evitar loop infinito
     #cumplimiento de cupo minimo
-    while not centros_cupos_minimos_completos(center_votant):
+    while not centros_cupos_minimos_completos(center_votant) or i<10:
         # una opcion es agarrar los centros que menos votantes tienen primero, o random
-        #for centro in sorted(center_votant.keys(), key=lambda x: len(center_votant[x])):
-        centro = random.randint(1,C)
-        if len(center_votant[centro]) > 0 and len(center_votant[centro]) < MIN_CUPO:
-            #vaciarlo
-            votantes = center_votant[centro]
-            while len(votantes): #los reasigno
-                for votant in votantes:
-                    order_dists = ordenar_distancias(dists[votant])
-                    for center,dist in order_dists:
-                        if len(center_votant[center]) < cupos[center] and len(center_votant[center]) > 0  and center != centro:
-                            center_votant[center].append(votant)
-                            votantes.remove(votant)
-                            break
+        centros_ordenados = sorted(center_votant.keys(), key=lambda x: len(center_votant[x]))
+        for centro in centros_ordenados:
+        #centro = random.randint(1,C)
+            if len(center_votant[centro]) > 0 and len(center_votant[centro]) < MIN_CUPO:
+                #vaciarlo
+                votantes = center_votant[centro]
+                while len(votantes): #los reasigno
+                    for votant in votantes:
+                        order_dists = ordenar_distancias(dists[votant])
+                        for center,dist in order_dists:
+                            if len(center_votant[center]) < cupos[center] and len(center_votant[center]) > 0  and center != centro:
+                                center_votant[center].append(votant)
+                                votantes.remove(votant)
+                                break
+        i += 1
     return center_votant
 
 
@@ -87,8 +97,12 @@ def main():
         for votante in votantes:
             distancias3.append(distancias2[(votante,centro)])
     
-    print(sum(distancias3) / len(distancias3) )
-    print(max(distancias3))
+    print("Promedio de las distancias: {}.".format(sum(distancias3) / len(distancias3) ))
+    print("Distancia maxima recorrida: {}.".format(max(distancias3)))
+    print("Distancia minima recorrida: {}.".format(min(distancias3)))
+    process = psutil.Process(os.getpid())
+    print("Memoria utilizada en bytes: {}.".format(process.memory_info().rss)) #medir memoria utilizada
+    
     
     
 if __name__ == "__main__":
